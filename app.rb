009a8ff5c31c7f51cf5c6e4eef6322bf453a87d3
Nicholas Keller <nic.keller328@gmail.com>
2020-05-06 11:11:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @number1 = params[:num1]
-    @number2 = params[:num2]
-    @number1 * @number2
+    @numbers = params[:num1, :num2]
+    @numbers[0] * @numbers[1]
   end
 end
